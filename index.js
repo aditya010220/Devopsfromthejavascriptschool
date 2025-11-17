15024bd02408a5f7bcd25b8c40c025cbf7e12e96
aditya010220 <am9962966@gmail.com>
2025-11-17 23:35:47 +0530

    feature of CD CI

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Welcome to DevOps from the JavaScript School!");
+
+
+console.log("This is the main entry point of the DevOps course.");
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("sample test file");
+setTimeout(() => {
+    console.log("This is a delayed message from the test file.");
+}, 2000);
+
+console.log("test completed");
